5e44cd30039d2bc48848cb946663c2e73106cc79
Fikret KÜRKLÜ <fikretkurklu@icloud.com>
2022-03-13 16:42:33 +0100

    finishing the front-end v4

--- front/components/WavyHeader.tsx
+++ front/components/WavyHeader.tsx
@@ -23,9 +23,9 @@ const WavyHeader: React.FC<WavyHeaderProps> = ({}) => {
             Bienvenue à l'ASSOCIATION REVE
           </h1>
           <p className="text-xl font-medium text-white">
-            Bienvenue sur le site internet de l’Association REVE. Vous voulez
-            découvrir et en apprendre plus sur qui sommes-nous, nos objectifs,
-            les activités que l’on organise... vous êtes à la bonne adresse !
+            Bienvenue sur notre site internet. Découvrir et en apprendre plus
+            sur qui sommes-nous, nos objectifs, les activités que l’on
+            organise... vous êtes à la bonne adresse !
           </p>
           <div className="mt-5 flex w-52 justify-between text-2xl text-white">
             <a target="_blank" href="https://instagram.com/associationreve">
