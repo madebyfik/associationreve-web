223b7b6fe2703ca5aaa1168adff575b31a247a98
Fikret KÜRKLÜ <fikretkurklu@icloud.com>
2022-03-13 16:05:33 +0100

    removing nav link

--- front/components/NavBar/NavBar.tsx
+++ front/components/NavBar/NavBar.tsx
@@ -58,7 +58,6 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
       >
         <div className="flex w-full flex-col items-start lg:ml-auto lg:inline-flex lg:h-auto  lg:w-auto lg:flex-row lg:items-center">
           <NavLink styleData={styles.navLink} link="/" title="Accueil" />
-          <NavLink styleData={styles.navLink} link="/contact" title="Contact" />
         </div>
       </div>
     </nav>
